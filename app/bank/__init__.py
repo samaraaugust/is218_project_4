5c8c096dc1c5bf4512767146026d4b30a1710e8c
samaraaugust <samaraaugust123@gmail.com>
2022-04-26 21:58:13 -0400

    added route for uploads and add to nav bar

---
+++ app/bank/__init__.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, abort
+from flask_login import login_required
+from app.bank.forms import csv_upload
+from jinja2 import TemplateNotFound
+bank = Blueprint('bank', __name__,
+                        template_folder='templates')
+
+@bank.route('/bank/upload', methods=['POST', 'GET'])
+@login_required
+def bank_upload():
+    form = csv_upload()
+    try:
+        return render_template('upload.html', form=form)
+    except TemplateNotFound:
+        abort(404)
