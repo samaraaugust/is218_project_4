7180d13bd949abdb8fe691c3422e9ed63e06c4ba
samaraaugust <samaraaugust123@gmail.com>
2022-04-26 23:28:08 -0400

    created endpoint and html to browse info

--- app/bank/__init__.py
+++ app/bank/__init__.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, abort, current_app
+from flask import Blueprint, render_template, abort, current_app, url_for
 from flask_login import login_required, current_user
 from app.bank.forms import csv_upload
 from jinja2 import TemplateNotFound
-from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename, redirect
 import os
 import csv
 from app.db.models import Bank
@@ -10,6 +10,18 @@ from app.db import db
 
 bank = Blueprint('bank', __name__,
                         template_folder='templates')
+
+@bank.route('/bank', methods=['GET'], defaults={"page": 1})
+@bank.route('/bank/<int:page>', methods=['GET'])
+def bank_browse(page):
+    page = page
+    per_page = 1000
+    pagination = Bank.query.paginate(page, per_page, error_out=False)
+    data = pagination.items
+    try:
+        return render_template('browse_transactions.html',data=data,pagination=pagination)
+    except TemplateNotFound:
+        abort(404)
 
 @bank.route('/bank/upload', methods=['POST', 'GET'])
 @login_required
@@ -26,6 +38,7 @@ def bank_upload():
                 list_of_transactions.append(Bank(row['AMOUNT'], row['TYPE']))
         current_user.bank = list_of_transactions
         db.session.commit()
+        return redirect(url_for('bank.bank_browse'))
     try:
         return render_template('upload.html', form=form)
     except TemplateNotFound:
