9df7ab540236438a0455c21e571b8967afede4ee
samaraaugust <samaraaugust123@gmail.com>
2022-05-01 14:28:26 -0400

    added test for registration and accessing dashboard

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -15,7 +15,10 @@ def test_auth_pages(client):
     assert response.status_code == 200
 
 
-
+def test_successful_register(client, auth):
+    """Successful register redirects to the login"""
+    response = auth.register()
+    assert response.headers["Location"] == "/login"
 
 
 def test_successful_login(client, auth):
@@ -23,3 +26,16 @@ def test_successful_login(client, auth):
     response2 = auth.register()
     response = auth.login()
     assert response.headers["Location"] == "/dashboard"
+
+def test_dashboard_logged_in(client, auth):
+    """If user is logged in can access dashboard page"""
+    response1 = auth.register()
+    response3 = auth.login()
+    response2 = client.get("/dashboard")
+    assert b"Welcome: test@email.com" in response2.data
+    assert b"Dashboard" in response2.data
+
+def test_dashboard_not_logged_in(client):
+    """if user is not logged in can not access dashboard gets sent back to login page"""
+    response = client.get("/dashboard")
+    assert response.headers["Location"] == "/login?next=%2Fdashboard"
