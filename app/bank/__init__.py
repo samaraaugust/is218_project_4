38427df4756cd6ca2ab98e0ab8a1c03424d61880
samaraaugust <samaraaugust123@gmail.com>
2022-05-01 14:18:59 -0400

    changed data tables to be per user

--- app/bank/__init__.py
+++ app/bank/__init__.py
@@ -5,7 +5,7 @@ from jinja2 import TemplateNotFound
 from werkzeug.utils import secure_filename, redirect
 import os
 import csv
-from app.db.models import Bank
+from app.db.models import Bank, User
 from app.db import db
 from sqlalchemy import select
 from sqlalchemy import create_engine
@@ -14,43 +14,29 @@ from sqlalchemy.orm import sessionmaker
 bank = Blueprint('bank', __name__,
                         template_folder='templates')
 
-@bank.route('/bank', methods=['GET'], defaults={"page": 1})
-@bank.route('/bank/<int:page>', methods=['GET'])
+@bank.route('/bank_datatables', methods=['GET'])
 @login_required
-def bank_browse(page):
+def browse_bank_datatables():
     engine = create_engine(current_app.config['SQLALCHEMY_DATABASE_URI'])
     Session = sessionmaker(bind=engine)
     session = Session()
-    page = page
-    per_page = 1000
-    pagination = Bank.query.paginate(page, per_page, error_out=False)
-    data = pagination.items
+    idNum = User.get_id(current_user)
+    data = session.query(Bank).filter(Bank.user_id == idNum).all()
     balance_debit = 0
-    data_sec = session.query(Bank).filter(Bank.type=='DEBIT').all()
+    data_sec = session.query(Bank).filter(Bank.type == 'DEBIT', Bank.user_id == idNum).all()
     for i in data_sec:
-        #print(i.amount)
         balance_debit = balance_debit + float(i.amount)
-    #print("D: ", balance_debit)
-    data_cred = session.query(Bank).filter(Bank.type=='CREDIT').all()
+    data_cred = session.query(Bank).filter(Bank.type == 'CREDIT', Bank.user_id == idNum).all()
     balance_credit = 0
     for p in data_cred:
-        #print(p.amount)
         balance_credit = balance_credit + float(p.amount)
-    #print("finalC: ", balance_credit)
     final_val = str(balance_credit + balance_debit)
-    #print("final Val: ", final_val)
-    #data_sec = Bank.query.all()
     session.close()
+
     try:
-        return render_template('browse_transactions.html',data=data,pagination=pagination, data_sec=data_sec, data_cred=data_cred, final_val=final_val)
+        return render_template('browse_bank_datatables.html',data=data, final_val=final_val)
     except TemplateNotFound:
         abort(404)
-
-@bank.route('/bank/account', methods=['GET'])
-def account_balence():
-    balence = 0
-    data = Bank.query.all()
-    print(data)
 
 
 
@@ -69,7 +55,7 @@ def bank_upload():
                 list_of_transactions.append(Bank(row['AMOUNT'], row['TYPE']))
         current_user.bank = list_of_transactions
         db.session.commit()
-        return redirect(url_for('bank.bank_browse'))
+        return redirect(url_for('bank.browse_bank_datatables'))
     try:
         return render_template('upload.html', form=form)
     except TemplateNotFound:
